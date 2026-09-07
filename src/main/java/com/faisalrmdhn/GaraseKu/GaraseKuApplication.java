d94ab8fcefe432f950f1b68f5964451d0c5de85b
Faisal Ramadhan <faisalramadhan1299@gmail.com>
2026-09-07 21:14:11 +0700

    First init project of GaraseKu website

---
+++ src/main/java/com/faisalrmdhn/GaraseKu/GaraseKuApplication.java
@@ -0,0 +1,13 @@
+package com.faisalrmdhn.GaraseKu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaraseKuApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GaraseKuApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/faisalrmdhn/GaraseKu/GaraseKuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faisalrmdhn.GaraseKu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaraseKuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
